caa323dfdce027347ad91667af7014dbb4bc3697
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2023-05-21 02:00:14 +0900

    Fix

--- mutex/index.js
+++ mutex/index.js
@@ -28,46 +28,35 @@ function msleep(n) {
 function main() {
   /**
    * 0 番目が 0 ならロックされてない、1 ならロックされてる
-   * @type {Uint8Array}
+   * @type {SharedArrayBuffer}
    */
-  const lock = new Uint8Array(new SharedArrayBuffer(1));
-
-  const sharedResource = new Uint8Array(new SharedArrayBuffer(4));
+  const sharedLock = new SharedArrayBuffer(1);
 
   if (worker_threads.isMainThread) {
     new worker_threads.Worker(new URL(import.meta.url), {
-      workerData: { name: "worker1" },
+      workerData: { name: "worker1", sharedLock },
     });
     new worker_threads.Worker(new URL(import.meta.url), {
-      workerData: { name: "worker2" },
+      workerData: { name: "worker2", sharedLock },
     });
   } else {
-    const { name } = workerData;
+    const { name, sharedLock } = workerData;
+    const lock = new Uint8Array(sharedLock);
     function retry() {
       if (testAndSet(lock)) {
         console.log(`${name} started something.`);
-        if (name === "worker1") {
-          sharedResource[0] = 0;
-          sharedResource[1] = 1;
-          sharedResource[2] = 2;
-          sharedResource[3] = 3;
-        } else if (name === "worker2") {
-          sharedResource[0] = 4;
-          sharedResource[1] = 3;
-          sharedResource[2] = 2;
-          sharedResource[3] = 1;
-        }
+        msleep(1000);
+        // ここでクリティカルセクション
         console.log(`${name} ended something.`);
       } else {
-        // retry 間隔が短すぎると Maximum call stack size なので 10ms 待つ
+        // retry 間隔が短すぎると Maximum call stack size になるので 10ms 待つ
         msleep(10);
+        retry();
       }
       tasRelease(lock);
     }
     retry();
   }
-
-  console.log(sharedResource);
 }
 
 main();
